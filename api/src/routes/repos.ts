7588692b77bcdd366ced43d09db8a470d2a9e1e2
Camila Rivera <camila.riverart@gmail.com>
2021-01-17 12:29:50 -0800

    Added verification in route repos to send only unique repositories by Id

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -6,16 +6,39 @@ export const repos = Router();
 
 const repoForkFilter = (repo: { fork: boolean }) => repo.fork === false;
 
+const aggregateAvoidingDuplicatesById = (
+  arrayA: Array<{ id: any }>,
+  arrayB: Array<{ id: any }>
+) => {
+  // Elements from arrayA will have priority over arrayB. In case of repetition of IDs within the repository, the first unique ID will be prioritized
+  const uniqueIDs = new Set();
+  const uniqueElementsByID: Array<{ id: any }> = [];
+
+  // Asumed data is not large, otherwise it shouldn't be copied to a new Array to iterate
+  [...arrayA, ...arrayB].forEach((element) => {
+    if (!uniqueIDs.has(element.id)) {
+      uniqueIDs.add(element.id);
+      uniqueElementsByID.push(element);
+    }
+  });
+
+  return uniqueElementsByID;
+};
+
 repos.get('/', async (_: Request, res: Response) => {
   res.header('Cache-Control', 'no-store');
   try {
     const response = await axios.get(
       'https://api.github.com/users/silverorange/repos'
     );
-    const internalRepos = reposBack.filter(repoForkFilter);
     const filteredResponse = response.data.filter(repoForkFilter);
-    const agregateRepos = [...internalRepos, ...filteredResponse];
-    res.send(agregateRepos);
+    const internalRepos = reposBack.filter(repoForkFilter);
+    const agregatedRepos = aggregateAvoidingDuplicatesById(
+      filteredResponse,
+      internalRepos
+    );
+
+    res.send(agregatedRepos);
   } catch (error) {
     res.status(500).send(`ERROR: ${error}`);
   }
