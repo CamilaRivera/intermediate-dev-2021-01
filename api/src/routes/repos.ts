553ef2b879a46069c0752ed7d7c48bc75ec6aa71
Camila Rivera <camila.riverart@gmail.com>
2021-01-17 02:38:44 -0800

    Refactored api/repos list endpoint:

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -4,21 +4,19 @@ import reposBack from '../../data/repos.json';
 
 export const repos = Router();
 
+const repoForkFilter = (repo: { fork: boolean }) => repo.fork === false;
+
 repos.get('/', async (_: Request, res: Response) => {
   res.header('Cache-Control', 'no-store');
   try {
     const response = await axios.get(
       'https://api.github.com/users/silverorange/repos'
     );
-    const internalRepos = reposBack.filter(
-      (repo: { fork: boolean }) => repo.fork === false
-    );
-    const filteredResponse = response.data.filter(
-      (repo: { fork: boolean }) => repo.fork === false
-    );
+    const internalRepos = reposBack.filter(repoForkFilter);
+    const filteredResponse = response.data.filter(repoForkFilter);
     const agregateRepos = [...internalRepos, ...filteredResponse];
     res.send(agregateRepos);
   } catch (error) {
-    console.error(error);
+    res.status(500).send(`ERROR: ${error}`);
   }
 });
