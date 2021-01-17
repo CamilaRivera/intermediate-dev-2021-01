4911f88e6f0a6e1bb8752ae53a8995c105cb5135
Camila Rivera <camila.riverart@gmail.com>
2021-01-16 20:28:57 -0800

    Added axios to dependencies. Updated endpoint to retrieve repos data.

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,12 +1,24 @@
 import { Router, Request, Response } from 'express';
+import axios from 'axios';
+import reposBack from '../../data/repos.json';
 
 export const repos = Router();
 
 repos.get('/', async (_: Request, res: Response) => {
   res.header('Cache-Control', 'no-store');
-
-  res.status(200);
-
-  // TODO: See README.md Task (A). Return repo data here. You’ve got this!
-  res.json([]);
+  try {
+    const response = await axios.get(
+      'https://api.github.com/users/silverorange/repos'
+    );
+    const internalRepos = reposBack.filter(
+      (repo: { fork: boolean }) => repo.fork === false
+    );
+    const filteredResponse = response.data.filter(
+      (repo: { fork: boolean }) => repo.fork === false
+    );
+    const agregateRepos = [...internalRepos, ...filteredResponse];
+    res.send(agregateRepos);
+  } catch (error) {
+    console.error(error);
+  }
 });
